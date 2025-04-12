28216d5f2322cb7a9d62d5064ff10158c4651b8c
neel <neelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.com>
2025-04-12 12:30:41 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 let bitcoin_product_id;
-let current_lot = 5;
+let current_lot = 20;
 let current_profit = 0;
 let total_profit = 0;
 let border_price;
@@ -23,7 +23,7 @@ let sell_response = null;
 let botRunning = true;
 let buy_sell_point = 50
 let buy_sell_profit_point = 100
-let cancel_gap = 70
+let cancel_gap = 50
 
 let order_exicuted_at_price = 0
 let project_error_message = ""
