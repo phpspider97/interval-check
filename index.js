81c4772ac9ede24e56d0c652f121bd99c9903cae
neel <neelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.comneelmani.sharma@writlabs.com>
2025-04-12 20:44:10 +0530

    update

--- index.js
+++ index.js
@@ -213,10 +213,10 @@ async function triggerOrder(current_price) {
       await createOrder('buy',current_price)
     }
     if (buy_response && current_price <= border_buy_price-cancel_gap) { //cancel existing buy order
-      const cancel = await cancelAllOpenOrder();
-      if (!cancel.status) return cancel;
       buy_response = null
       current_lot *= lot_size_increase
+      const cancel = await cancelAllOpenOrder();
+      if (!cancel.status) return cancel;
     }
 
     if (!sell_response && current_price < border_sell_price) { // sell order
@@ -226,10 +226,10 @@ async function triggerOrder(current_price) {
       await createOrder('sell',current_price)
     }
     if (sell_response && current_price >= border_sell_price+cancel_gap) { // cancel existing sell order
-      const cancel = await cancelAllOpenOrder();
-      if (!cancel.status) return cancel;
       sell_response = null;
       current_lot *= lot_size_increase
+      const cancel = await cancelAllOpenOrder();
+      if (!cancel.status) return cancel;
     }
 
     if (current_price > border_buy_profit_price || current_price < border_sell_profit_price) { // exit when acheive target
